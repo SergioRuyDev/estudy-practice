d3ab835777e5605dbaacad1cad23ae6b3e1fd885
sergioruydev <sergioruyenator@gmail.com>
2023-03-17 17:32:36 -0300

    Some Hacker Rank exercises.

--- src/main/java/educative/CheckMergeArray.java
+++ src/main/java/educative/CheckMergeArray.java
@@ -4,42 +4,49 @@ package educative;
 //        In this problem, given two sorted arrays, you have to implement the int[] mergeArrays(int[] arr1, int[] arr2)
 //        method, which returns an array consisting of all elements of both arrays in a sorted way.
 
+import java.util.Arrays;
+
 public class CheckMergeArray {
     public static void main(String[] args) {
-        int [] array = new int [] {90, 109, 67, 34};
-        int [] array2 = new int [] { 23, 5, 12, 22 };
-        mergeArrays(array, array2);
+        int[] arr1 = {1,12,14,17,23}; // creating a sorted array called arr1
+        int[] arr2 = {11,19,27};  // creating a sorted array called arr2
+
+        int[] resultantArray = mergeArrays(arr1, arr2); // calling mergeArrays
+
+        System.out.print("Arrays after merging: ");
+        for(int i = 0; i < arr1.length + arr2.length; i++) {
+            System.out.print(resultantArray[i] + " ");
+        }
 
     }
 
     public static int[] mergeArrays(int[] arr1, int[] arr2) {
         int s1 = arr1.length;
         int s2 = arr2.length;
-        int [] arr3 = new int[s1+s2];
+        int[] resultantArray = new int[s1+s2];
         int i = 0, j = 0, k = 0;
 
         // Traverse both array
-        while (i<s1 && j <s2)
-        {
+        while (i < s1 && j < s2) {
             // Check if current element of first
             // array is smaller than current element
             // of second array. If yes, store first
             // array element and increment first array
             // index. Otherwise do same with second array
             if (arr1[i] < arr2[j])
-                arr3[k++] = arr1[i++];
+                resultantArray[k++] = arr1[i++];
             else
-                arr3[k++] = arr2[j++];
+                resultantArray[k++] = arr2[j++];
         }
 
         // Store remaining elements of first array
         while (i < s1)
-            arr3[k++] = arr1[i++];
+            resultantArray[k++] = arr1[i++];
 
         // Store remaining elements of second array
         while (j < s2)
-            arr3[k++] = arr2[j++];
+            resultantArray[k++] = arr2[j++];
 
-        return arr3;
+        return resultantArray;
     }
 }
---
+++ src/main/java/educative/CheckSum.java
@@ -0,0 +1,13 @@
+package educative;
+
+public class CheckSum {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] findSum(int[] arr, int n) {
+        int[] result = new int[2];
+        // write your code here
+        return result;   // return the elements in the array whose sum is equal to the value passed as parameter
+    }
+}
