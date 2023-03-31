52314e5a91cb548b18ccfbed52ab9b417ee76585
sergioruydev <sergioruyenator@gmail.com>
2023-03-31 16:54:01 -0300

    Some Educative.io exercises.

---
+++ src/main/java/educative/NearestPowerOfTwo.java
@@ -0,0 +1,14 @@
+package educative;
+
+import java.util.Scanner;
+
+public class NearestPowerOfTwo {
+    public static void main(String[] args) {
+
+        int num = 0;
+        int pwr = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+    }
+}
