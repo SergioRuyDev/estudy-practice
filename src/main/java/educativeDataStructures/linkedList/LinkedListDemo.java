e306a5b26b4f9af6706604c7f093428e56c08d9c
sergioruydev <sergioruyenator@gmail.com>
2023-04-22 16:53:11 -0300

    Some Educative.io exercises.

--- src/main/java/educativeDataStructures/linkedList/LinkedListDemo.java
+++ src/main/java/educativeDataStructures/linkedList/LinkedListDemo.java
@@ -1,6 +1,7 @@
 package educativeDataStructures.linkedList;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class LinkedListDemo {
@@ -12,6 +13,7 @@ public class LinkedListDemo {
         linkedList.add(1);
         linkedList.add(2);
         linkedList.addLast(3);
+        linkedList.addLast(2);
         System.out.println(linkedList);
 
         linkedList.addFirst(10);
@@ -29,6 +31,26 @@ public class LinkedListDemo {
         System.out.println(list);
 
         linkedList.addAll(0, list);
+        System.out.println(linkedList);
+
+        System.out.println(linkedList.getFirst());
+        System.out.println(linkedList.getLast());
+        System.out.println(linkedList.get(3));
+
+        linkedList.remove(); //Removes the first element.
+        System.out.println("LinkedList after removing first element " + linkedList);
+
+        linkedList.removeLast(); //Removes the last element.
+        System.out.println("LinkedList after removing last element " + linkedList);
+
+        linkedList.remove(new Integer(2)); //Removes the first occurrence of 2.
+        System.out.println("LinkedList after removing first occurrence of 2. " + linkedList);
+
+        linkedList.removeLastOccurrence(new Integer(2)); //Removes the last occurrence of 4.
+        System.out.println("LinkedList after removing the last occurrence of 4. " + linkedList);
+
+        Collections.sort(linkedList);
+
         System.out.println(linkedList);
     }
 }
