585c5bc79cddd71c6405ceb5d930a3b7833d863d
sergioruydev <sergioruyenator@gmail.com>
2023-05-19 16:32:11 -0300

    Stream module.

--- src/main/java/java8estudy/stream/StreamFilter.java
+++ src/main/java/java8estudy/stream/StreamFilter.java
@@ -7,7 +7,7 @@ public class StreamFilter {
     public static void main(String[] args) {
 
         List<Player> list = new ArrayList<>();
-        list.add(new Player("Zidane", 41));
+        list.add(new Player("Zidane", 17));
         list.add(new Player("Ronaldo", 25));
         list.add(new Player("Messi", 38));
         list.add(new Player("Rodrigo", 21));
@@ -17,6 +17,14 @@ public class StreamFilter {
 
         list.stream()
                 .filter(players -> players.getAge() < 30 && players.getAge() > 21)
+                .forEach(System.out::println);
+
+        //In the above example, we wrote all the conditions in a single filter.
+        //We can also chain the filter method to make the code more readable.
+        list.stream()
+                .filter(player -> player.getName() != null ) // Filtering the object where name is not null
+                .filter(player -> player.getAge() > 18 ) // Filtering the objects where age is greater than 18
+                .filter(player -> player.getAge() < 30) // Filtering the objects where age is less than 60
                 .forEach(System.out::println);
     }
 }
