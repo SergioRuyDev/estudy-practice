d7d8b306007b2f809c0b4891d57c252f629238c2
SergioRuyDev <sergioruyenator@gmail.com>
2024-03-03 17:25:13 -0300

    sliding window

--- src/main/java/leetcode/TwoPointer.java
+++ src/main/java/leetcode/TwoPointer.java
@@ -49,6 +49,28 @@ public class TwoPointer {
         return true;
     }
 
+    public static boolean checkIntIfPalindrome(int x) {
+        // If x is negative or ends with 0 but is not 0, it's not a palindrome
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversed = 0;
+        int original = x;
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            // Check for integer overflow
+            if (reversed > Integer.MAX_VALUE/10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return false;
+            }
+            reversed = reversed * 10 + digit;
+        }
+
+        return original == reversed;
+    }
+
     public static boolean checkSumPairs(int[] nums, int target) {
 
         int left = 0;
